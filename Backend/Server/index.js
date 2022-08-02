f9f7cd404c5609ae13d239ef15728ed2eaf3b58e
Vince P <32515747+Peperito@users.noreply.github.com>
2022-08-02 19:46:56 +0200

    "Added POST and PUT for users"

--- Backend/Server/index.js
+++ Backend/Server/index.js
@@ -16,6 +16,12 @@ app.use(
 
 app.get('/users', db.getUsers);
 
+app.get('users/:id', db.getUserById);
+
+app.post('/users', db.createUser);
+
+app.put('/users/:id', db.updateUser);
+
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API for e-commerce Project' })
 })
--- Backend/Server/queries.js
+++ Backend/Server/queries.js
@@ -10,7 +10,7 @@ const pool = new Pool({
 });
 
 const getUsers = (request, response) => {
-    pool.query('SELECT first_name, last_name FROM users ORDER BY id ASC', (error, results) => {
+    pool.query('SELECT first_name, last_name, created_at, modified_at FROM users ORDER BY id ASC', (error, results) => {
       if (error) {
         throw error
       }
@@ -18,6 +18,43 @@ const getUsers = (request, response) => {
     })
 };
 
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+};
+
+const createUser = (request, response) => {
+  const { first_name, last_name, email } = request.body
+
+  pool.query('INSERT INTO users (first_name, last_name, email) VALUES ($1, $2, $3) RETURNING *', [first_name, last_name, email], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+  })
+};
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { first_name, last_name, email } = request.body
+
+  pool.query(
+    'UPDATE users SET first_name = $1, last_name = $2, email = $3 WHERE id = $4',
+    [first_name, last_name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
 
 
-module.exports = {getUsers};
+module.exports = {getUsers, getUserById, createUser, updateUser};
